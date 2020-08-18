0d35d0a25393b21b2bfec525d8a7c6d3ea5bf530
arglinguist <arglinguist@gmail.com>
2020-08-17 20:11:08 -0700

    added numbers to each div

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let container = document.getElementById("container");
 
 for (let i=0; i<16; i++){
     let divRow = document.createElement('div');
-    divRow.classList.add("squareDiv");
+    divRow.classList.add("squareDiv"); //might need to be removed?
     
     for(let n=0; n<16; n++){
         let divColumn = document.createElement('div');
         divColumn.classList.add("squareDiv");
         divColumn.setAttribute('style', 'float: left');
+        divColumn.textContent=(i+1)*(n+1);
         container.appendChild(divColumn);
     }
     container.appendChild(divRow);
