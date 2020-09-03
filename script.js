1c7935db4b33226c1c9fce1e20fe4849ddb62389
arglinguist <arglinguist@gmail.com>
2020-09-02 17:55:26 -0700

    made grid generation into funciton that accepts a numeric parameter

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+
+
 let container = document.getElementById("container");
 
-for (let i=0; i<16; i++){
-    let divRow = document.createElement('div');
-    divRow.classList.add("rowDiv"); //might need to be removed?
-    
-    for(let n=0; n<16; n++){
-        let divColumn = document.createElement('div');
-        divColumn.classList.add("squareDiv");
-        divColumn.setAttribute('style', 'float: left');
-        //divColumn.textContent=(i+1)*(n+1);
-        container.appendChild(divColumn);
+function generateGrid(selectedSize){
+    for (let i=0; i<selectedSize; i++){
+        let divRow = document.createElement('div');
+        divRow.classList.add("rowDiv"); //might need to be removed?
+        
+        for(let n=0; n<selectedSize; n++){
+            let divColumn = document.createElement('div');
+            divColumn.classList.add("squareDiv");
+            divColumn.setAttribute('style', 'float: left');
+            //divColumn.textContent=(i+1)*(n+1);
+            container.appendChild(divColumn);
+        }
+        container.appendChild(divRow);
     }
-    container.appendChild(divRow);
 }
 
+generateGrid(32);
+let selectedColor = "black";
 
 let divList=Array.from(document.getElementsByClassName('squareDiv'));
-
-let selectedColor = prompt("What color do you want?");
 
 divList.forEach(div=>div.addEventListener('mouseover', e => {
     activateDiv(div,selectedColor);
